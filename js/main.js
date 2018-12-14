685c4b0708f473816a3c208f397e897ddb6c02c2
R3l3ntl3ss <dev.91096@gmail.com>
2018-12-14 22:04:30 +0530

    Completed basic layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-const el = document.querySelector('.text')
-const fx = new TextScramble(el)
+const salutation = [
+    'Hey',
+    'Hola',
+    'Hello',
+    'Konnichiwa'
+]
 
-fx.setText('Hey, My name is Dev')
+
+const sal = document.querySelector('.salutation')
+const sal_class = new TextScramble(sal)
+
+const content = document.querySelector('.content')
+const content_class = new TextScramble(content)
+
+const link = document.querySelector('.link')
+const link_class = new TextScramble(link)
+
+let counter = 0
+const salutation_fx = () => {
+    sal_class.setText(salutation[counter]).then(() => {
+        setTimeout(salutation_fx, 3000)
+    })
+    counter = (counter + 1) % salutation.length
+}
+
+const content_fx = () => {
+    content_class.setText('What Up?')
+}
+
+const link_fx = () => {
+    link_class.setText('github')
+}
+
+salutation_fx()
+// setTimeout(content_fx, 6000)
+setTimeout(link_fx, 6000)
