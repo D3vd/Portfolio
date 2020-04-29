5d854a22422919480448b0a4277ebcb186a69df1
Dev Daksan <dev.91096@gmail.com>
2020-04-29 14:39:34 +0530

    Added Helmet and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`],
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+
+  siteMetadata: {
+    title: "Dev Daksan",
+    description:
+      "Hey, my name is Dev. I am an aspiring software engineer. I spend most of my free time contributing to open source and making stuff that no one asked for.",
+    keywords:
+      "full stack developer, frontend developer, portfolio, personal website",
+  },
 }
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,25 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+
+import useSiteMetadata from "../../hooks/use-sitemetadata"
 
 import "./global.scss"
 
 function layout({ children }) {
-  return <div>{children}</div>
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { title, description, keywords } = useSiteMetadata()
+
+  return (
+    <>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Helmet>
+      <main>{children}</main>
+    </>
+  )
 }
 
 export default layout
